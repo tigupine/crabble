bfab5675ca2e4d2943ddaa199a64f384a797138a
tigupine <49821469+tigupine@users.noreply.github.com>
2021-11-14 03:11:58 -0800

    Add files via upload
    Add support for blanks. It really makes the game chug when it thinks what to do with one, but it's tolerable.
    
    Update the word list to remove the words that have been expurgated for tournament play (don't want those showing up in demos!)

--- crabble.py
+++ crabble.py
@@ -1,5 +1,6 @@
 import itertools
 import random
+import string
 
 RACK_SIZE = 5
 # 1 = double word, 2 = double letter, 3 = triple letter
@@ -12,52 +13,31 @@ BOARD = [[1,0,0,0,2,0,0,0,1],
          [0,0,3,0,0,0,3,0,0],
          [0,1,0,0,0,0,0,1,0],
          [1,0,0,0,2,0,0,0,1]]
-"""
-BOARD = [[1,0,0,0,2,0,2,0,0,0,1],
-         [0,1,0,0,0,2,0,0,0,1,0],
-         [0,0,1,0,0,0,0,0,1,0,0],
-         [0,0,0,3,0,0,0,3,0,0,0],
-         [0,0,0,0,2,0,2,0,0,0,0],
-         [0,2,0,0,0,1,0,0,0,2,0],
-         [0,0,0,0,2,0,2,0,0,0,0],
-         [0,0,0,3,0,0,0,3,0,0,0],
-         [0,0,1,0,0,0,0,0,1,0,0],
-         [0,1,0,0,0,2,0,0,0,1,0],
-         [1,0,0,0,2,0,2,0,0,0,1]]
-"""
+
 BOARD_SIZE = len(BOARD)
 
-# TODO: handle blanks? or not
 VALUES = {'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4,
           'G': 2, 'H': 4, 'I': 1, 'J': 8, 'K': 5, 'L': 1,
           'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1,
           'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8,
-          'Y': 4, 'Z': 10}
+          'Y': 4, 'Z': 10, '?': 0}
 
 # Use about 1/3 the standard Scrabble frequencies since the board size is
 # about 1/3 of the original. (Actually 40 tiles)
-# TODO: replace one A with a blank?
-FREQS = {'A': 4, 'B': 1, 'C': 1, 'D': 1, 'E': 4, 'F': 1,
+FREQS = {'A': 3, 'B': 1, 'C': 1, 'D': 1, 'E': 4, 'F': 1,
          'G': 1, 'H': 1, 'I': 3, 'J': 1, 'K': 1, 'L': 1,
          'M': 1, 'N': 2, 'O': 3, 'P': 1, 'Q': 1, 'R': 2,
          'S': 2, 'T': 2, 'U': 1, 'V': 1, 'W': 1, 'X': 1,
-         'Y': 1, 'Z': 1}
-
-"""
-# 1/2 the standard freqs
-FREQS = {'A': 4, 'B': 1, 'C': 1, 'D': 2, 'E': 6, 'F': 1,
-         'G': 1, 'H': 1, 'I': 4, 'J': 1, 'K': 1, 'L': 2,
-         'M': 1, 'N': 3, 'O': 4, 'P': 1, 'Q': 1, 'R': 3,
-         'S': 2, 'T': 3, 'U': 2, 'V': 1, 'W': 1, 'X': 1,
-         'Y': 1, 'Z': 1}
-"""
+         'Y': 1, 'Z': 1, '?': 1}
 
 # Bonus for using entire rack
 BINGO_BONUS = 15
 
 f = open("words.txt", "r")
-# TODO: expurgate
 WORDS = set([l.strip() for l in f.readlines() if len(l) <= BOARD_SIZE + 1])
+
+def value(l):
+    return VALUES.get(l, 0)
 
 def undo_edits(board, edits):
     for rr, cc, _ in edits:
@@ -76,49 +56,47 @@ def check_board(board, edits, across, r, c, first_play):
     total_score = 0
     scored_words = []
     connected = False
+    edit_positions = set([(r, c) for r, c, _ in edits])
     for i, across in checks:
         dr, dc = (0, 1) if across else (1, 0)
-        w, score, multiplier, new_play = '', 0, 1, False
+        w, score, multiplier, has_old, has_new = '', 0, 1, False, False
         rr, cc = i * dc, i * dr
-        new_play = False
         while rr <= BOARD_SIZE and cc <= BOARD_SIZE:
             l = False
             if rr != BOARD_SIZE and cc != BOARD_SIZE:
                 l = board[rr][cc]
             if l:
                 w += l
-                if l.islower():
-                    new_play = True
+                if (rr, cc) in edit_positions:
+                    has_new = True
                     m = BOARD[rr][cc]
                     if m == 1:
                         multiplier *= 2
-                        score += VALUES[l.upper()]
+                        score += value(l)
                     else:
-                        score += VALUES[l.upper()] * (m if m != 0 else 1)
+                        score += value(l) * (m if m != 0 else 1)
                 else:
-                    score += VALUES[l]
+                    has_old = True
+                    score += value(l)
             elif w:
                 if len(w) >= 2:
-                    # If the word has both uppercase and lowercase letters, it
-                    # uses both new tiles and tiles that were on the board, so the
-                    # board is connected.
-                    if not w.isupper() and not w.islower():
+                    if has_old and has_new:
                         connected = True
-                    w = w.upper()
-                    if w not in WORDS:
+                    if w.upper() not in WORDS:
                         return False, None
-                    if new_play:
+                    if has_new:
                         total_score += score * multiplier
                         if len(edits) == RACK_SIZE:
                             total_score += BINGO_BONUS
                         scored_words.append(w)
-                w, score, multiplier, new_play = '', 0, 1, False
+                w, score, multiplier, has_old, has_new = (
+                 '', 0, 1, False, False)
             rr, cc = rr + dr, cc + dc
     if not connected and not first_play:
         return False, None
     return total_score, scored_words
 
-def check_play(board, r, c, d, p, first_turn):
+def check_play(board, r, c, d, p, first_play):
     edits = []
     new_pos = []
     rr, cc = r, c
@@ -130,12 +108,13 @@ def check_play(board, r, c, d, p, first_turn):
         if rr == BOARD_SIZE or cc == BOARD_SIZE:
             undo_edits(board, edits)
             return False, None, None
-        board[rr][cc] = p[i].lower()
+        board[rr][cc] = p[i]
         edits.append((rr, cc, p[i]))
-    if first_turn and not board[BOARD_SIZE//2][BOARD_SIZE//2]:
+    if first_play and not board[BOARD_SIZE//2][BOARD_SIZE//2]:
         undo_edits(board, edits)
         return False, None, None
-    score, scored_words = check_board(board, edits, (dc == 1), r, c, first_turn)
+    score, scored_words = check_board(
+        board, edits, (dc == 1), r, c, first_play)
     undo_edits(board, edits)
     return score, edits, scored_words
     
@@ -145,7 +124,12 @@ def find_plays(board, rack, first_play):
     for n in range(1, RACK_SIZE + 1):
         for c in itertools.combinations(rack, n):
             for p in itertools.permutations(c):
-                plays.add(p)
+                if '?' in p:
+                    blank_index = p.index('?')
+                    for l in string.ascii_lowercase:
+                        plays.add(p[0:blank_index] + (l,) + p[blank_index+1:])
+                else:
+                    plays.add(p)
     for r in range(BOARD_SIZE):
         for c in range(BOARD_SIZE):
             if board[r][c]:
@@ -154,9 +138,9 @@ def find_plays(board, rack, first_play):
                 for p in plays:
                     score, edits, scored_words = check_play(
                         board, r, c, d, p, first_play)
+                    # Since we only have 1 blank, it is impossible to score 0.
                     if score:
-                        valid.append(
-                            (score, r, c, p, edits, scored_words))
+                        valid.append((score, r, c, p, edits, scored_words))
     return valid
 
 def draw(bag, rack):
@@ -204,11 +188,15 @@ def sim(log=False):
         plays = find_plays(board, racks[active], first_play)
         if plays:
             score, r, c, p, edits, scored_words = random.choice(plays)
+            played_blank = False
             for rr, cc, l in edits:
                 board[rr][cc] = l
             old_rack = racks[active][:]
             for l in p:
-                racks[active].pop(racks[active].index(l))
+                if l.islower():
+                    racks[active].remove('?')
+                else:
+                    racks[active].remove(l)
             scores[active] += score
             scoreline = "rack {}, played r{}c{} {}  score {}".format(
                 rep_rack(old_rack), r+1, c+1, rep_words(scored_words), score)
@@ -244,8 +232,9 @@ def sim(log=False):
                     scores[0], scores[1], p+1, rep_rack(racks[p]), penalty))
     return scores
 
-# sim(log=True)
+sim(log=True)
 
+"""
 wins = [0, 0]
 for i in range(10000):
     print(i)
@@ -257,3 +246,4 @@ for i in range(10000):
         wins[1] += 1
 
 print(wins)
+"""
