b199e37135161af0f4764b4259052e25a6c34aab
tigupine <49821469+tigupine@users.noreply.github.com>
2021-11-14 04:03:57 -0800

    Add files via upload
    Add framework for strategies

--- crabble.py
+++ crabble.py
@@ -1,6 +1,10 @@
+from collections import namedtuple
 import itertools
 import random
-import string
+from string import ascii_lowercase
+
+ValidPlay = namedtuple(
+    'ValidPlay', ['score', 'r', 'c', 'p', 'edits', 'scored_words'])
 
 RACK_SIZE = 5
 # 1 = double word, 2 = double letter, 3 = triple letter
@@ -22,8 +26,7 @@ VALUES = {'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4,
           'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8,
           'Y': 4, 'Z': 10, '?': 0}
 
-# Use about 1/3 the standard Scrabble frequencies since the board size is
-# about 1/3 of the original. (Actually 40 tiles)
+# 40 tiles
 FREQS = {'A': 3, 'B': 1, 'C': 1, 'D': 1, 'E': 4, 'F': 1,
          'G': 1, 'H': 1, 'I': 3, 'J': 1, 'K': 1, 'L': 1,
          'M': 1, 'N': 2, 'O': 3, 'P': 1, 'Q': 1, 'R': 2,
@@ -36,14 +39,16 @@ BINGO_BONUS = 15
 f = open("words.txt", "r")
 WORDS = set([l.strip() for l in f.readlines() if len(l) <= BOARD_SIZE + 1])
 
-def value(l):
-    return VALUES.get(l, 0)
+# Options for strategies to return.
+PLAY = 'PLAY'
+EXCHANGE = 'EXCHANGE'
+PASS = 'PASS'
 
-def undo_edits(board, edits):
-    for rr, cc, _ in edits:
-        board[rr][cc] = False
+# TODO: write tests for these functions
 
 def check_board(board, edits, across, r, c, first_play):
+    def value(l):
+        return VALUES.get(l, 0)
     checks = set()
     if across:
         checks.add((r, True))
@@ -78,7 +83,7 @@ def check_board(board, edits, across, r, c, first_play):
                 else:
                     has_old = True
                     score += value(l)
-            elif w:
+            elif w:  # reached the end of a word, process it
                 if len(w) >= 2:
                     if has_old and has_new:
                         connected = True
@@ -92,11 +97,15 @@ def check_board(board, edits, across, r, c, first_play):
                 w, score, multiplier, has_old, has_new = (
                  '', 0, 1, False, False)
             rr, cc = rr + dr, cc + dc
+    # Only the first play of the game is allowed to be disconnected.
     if not connected and not first_play:
         return False, None
     return total_score, scored_words
 
 def check_play(board, r, c, d, p, first_play):
+    def undo_edits(board, edits):
+        for rr, cc, _ in edits:
+            board[rr][cc] = False
     edits = []
     new_pos = []
     rr, cc = r, c
@@ -118,15 +127,15 @@ def check_play(board, r, c, d, p, first_play):
     undo_edits(board, edits)
     return score, edits, scored_words
     
-def find_plays(board, rack, first_play):
+def find_valid_plays(board, rack, first_play):
     plays = set()
-    valid = []
+    valid_plays = []
     for n in range(1, RACK_SIZE + 1):
         for c in itertools.combinations(rack, n):
             for p in itertools.permutations(c):
                 if '?' in p:
                     blank_index = p.index('?')
-                    for l in string.ascii_lowercase:
+                    for l in ascii_lowercase:
                         plays.add(p[0:blank_index] + (l,) + p[blank_index+1:])
                 else:
                     plays.add(p)
@@ -140,8 +149,9 @@ def find_plays(board, rack, first_play):
                         board, r, c, d, p, first_play)
                     # Since we only have 1 blank, it is impossible to score 0.
                     if score:
-                        valid.append((score, r, c, p, edits, scored_words))
-    return valid
+                        valid_plays.append(
+                            ValidPlay(score, r, c, p, edits, scored_words))
+    return valid_plays
 
 def draw(bag, rack):
     while bag and len(rack) < RACK_SIZE:
@@ -164,10 +174,37 @@ def rep_rack(rack):
     return ''.join(rack)
 
 def rep_words(scored_words):
-    return ', '.join(scored_words) 
+    return ','.join(scored_words) 
 
-# TODO: this should take two strategies as arguments
-def sim(log=False):
+# ********** BEGIN STRATEGIES **********
+
+# A strategy takes the following inputs:
+# valid_plays: list of all valid plays, each as a ValidPlay tuple
+# board: the current board
+# rack: the active player's rack
+# bag: the remaining contents of the bag (sorted)
+# TODO: will probably need more inputs like current scores
+# TODO: maybe better to pass around game state objects?
+# TODO: these should probably also be objects
+def random_strat(valid_plays, board, rack, bag):
+    if valid_plays:
+        return PLAY, random.choice(valid_plays)
+    else:
+        return PASS, None
+
+def greedy_strat(valid_plays, board, rack, bag):
+    if valid_plays:
+        valid_plays.sort()
+        valid_plays.reverse()
+        return PLAY, valid_plays[0]
+    else:
+        return EXCHANGE, None
+
+# TODO: write more strats (e.g., lookahead)
+
+# ********** END STRATEGIES **********
+
+def sim(strat1, strat2, log=False):
     board = [[False for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
     bag = []
     for l, f in FREQS.items():
@@ -178,6 +215,7 @@ def sim(log=False):
     draw(bag, racks[0])
     draw(bag, racks[1])
     scores = [0, 0]
+    strats = [strat1, strat2]
     active = 0  # 0 on first player's turn, 1 on second player's turn
     first_play = True  # whether or not board is empty
     if log:
@@ -185,9 +223,11 @@ def sim(log=False):
     wordless_turns = 0
     while racks[0] and racks[1]:
         # TODO: handle exchanges as possible moves
-        plays = find_plays(board, racks[active], first_play)
-        if plays:
-            score, r, c, p, edits, scored_words = random.choice(plays)
+        valid_plays = find_valid_plays(board, racks[active], first_play)
+        choice, details = strats[active](
+            valid_plays, board, racks[active], sorted(bag))
+        if choice == PLAY:
+            score, r, c, p, edits, scored_words = details
             played_blank = False
             for rr, cc, l in edits:
                 board[rr][cc] = l
@@ -203,24 +243,25 @@ def sim(log=False):
             draw(bag, racks[active])
             wordless_turns = 0
             first_play = False
-        else:
+        elif choice == EXCHANGE and bag:
             wordless_turns += 1
-            if wordless_turns == 6:
-                break
             if bag:
                 oldrack = racks[active][:]
-                # currently just exchange everything
+                # currently just exchange everything. TODO: exchange specifics
                 exchange(bag, racks[active], racks[active])
                 scoreline = "rack {}, redrew to {}".format(
                     rep_rack(oldrack), rep_rack(racks[active]))
-            else:
-                scoreline = "rack {}, passed".format(rep_rack(racks[active]))
+        else: # PASS
+            wordless_turns += 1
+            scoreline = "rack {}, passed".format(rep_rack(racks[active]))
         if log:
             print("{}-{}   P{} {}".format(
                 scores[0], scores[1], active+1, scoreline))
-            if plays:
+            if choice == PLAY:
                 print(rep_board(board))
             print("")
+        if wordless_turns == 6:
+            break
         active = 1 - active
 
     for p in range(2):
@@ -232,18 +273,32 @@ def sim(log=False):
                     scores[0], scores[1], p+1, rep_rack(racks[p]), penalty))
     return scores
 
-sim(log=True)
+def compare_strats(strat1, strat2, num_trials):
+    assert num_trials % 2 == 0, "Number of trials must be even"
+    wins = [0, 0]
+    score_totals = [0, 0]
+    strats = [strat1, strat2]
+    for i in range(num_trials):
+        goes_first = i % 2
+        scores = sim(strats[goes_first], strats[1-goes_first])
+        if goes_first == 0:
+            score1, score2 = scores
+        else:
+            score2, score1 = scores
+        print("Game {} ({} goes first): P1 {} P2 {}".format(
+            i+1, goes_first+1, score1, score2))
+        if score1 > score2:
+            wins[0] += 1
+        elif score2 > score1:
+            wins[1] += 1
+        else:
+            wins[0] += 0.5
+            wins[1] += 0.5
+        score_totals[0] += score1
+        score_totals[1] += score2
+    # TODO: print names of strats
+    print("Player 1 won {}, Player 2 won {}".format(wins[0], wins[1]))
+    print("Average scores: Player 1 {}, Player 2 {}".format(
+        score_totals[0] / num_trials, score_totals[1] / num_trials))
 
-"""
-wins = [0, 0]
-for i in range(10000):
-    print(i)
-    score1, score2 = sim()
-    print(score1, score2)
-    if score1 > score2:
-        wins[0] += 1
-    else:
-        wins[1] += 1
-
-print(wins)
-"""
+compare_strats(random_strat, greedy_strat, 100)
