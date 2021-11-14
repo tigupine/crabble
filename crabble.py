5ea126e9017cfff5e39772c8d0accd6fe00876ba
tigupine <49821469+tigupine@users.noreply.github.com>
2021-11-14 14:17:10 -0800

    Reorg, bug fixes
    Check for valid lanes / numbers of tiles to play before trying specific tiles.

--- crabble.py
+++ crabble.py
@@ -4,21 +4,21 @@ import random
 from string import ascii_lowercase
 
 ValidPlay = namedtuple(
-    'ValidPlay', ['score', 'r', 'c', 'p', 'edits', 'scored_words'])
+    'ValidPlay', ['score', 'r', 'c', 'edits', 'scored_words'])
 
 RACK_SIZE = 5
 # 1 = double word, 2 = double letter, 3 = triple letter
-BOARD = [[1,0,0,0,2,0,0,0,1],
-         [0,1,0,0,0,0,0,1,0],
-         [0,0,3,0,0,0,3,0,0],
-         [0,0,0,2,0,2,0,0,0],
-         [2,0,0,0,1,0,0,0,2],
-         [0,0,0,2,0,2,0,0,0],
-         [0,0,3,0,0,0,3,0,0],
-         [0,1,0,0,0,0,0,1,0],
-         [1,0,0,0,2,0,0,0,1]]
+PREMIUMS = [[1,0,0,0,2,0,0,0,1],
+            [0,1,0,0,0,0,0,1,0],
+            [0,0,3,0,0,0,3,0,0],
+            [0,0,0,2,0,2,0,0,0],
+            [2,0,0,0,1,0,0,0,2],
+            [0,0,0,2,0,2,0,0,0],
+            [0,0,3,0,0,0,3,0,0],
+            [0,1,0,0,0,0,0,1,0],
+            [1,0,0,0,2,0,0,0,1]]
 
-BOARD_SIZE = len(BOARD)
+BOARD_SIZE = len(PREMIUMS)
 
 VALUES = {'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4,
           'G': 2, 'H': 4, 'I': 1, 'J': 8, 'K': 5, 'L': 1,
@@ -27,14 +27,15 @@ VALUES = {'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4,
           'Y': 4, 'Z': 10, '?': 0}
 
 # 40 tiles
-FREQS = {'A': 3, 'B': 1, 'C': 1, 'D': 1, 'E': 4, 'F': 1,
+# either do 3 As and one ?, or 4 As
+FREQS = {'A': 4, 'B': 1, 'C': 1, 'D': 1, 'E': 4, 'F': 1,
          'G': 1, 'H': 1, 'I': 3, 'J': 1, 'K': 1, 'L': 1,
          'M': 1, 'N': 2, 'O': 3, 'P': 1, 'Q': 1, 'R': 2,
          'S': 2, 'T': 2, 'U': 1, 'V': 1, 'W': 1, 'X': 1,
-         'Y': 1, 'Z': 1, '?': 1}
+         'Y': 1, 'Z': 1, '?': 0}
 
 # Bonus for using entire rack
-BINGO_BONUS = 15
+BINGO_BONUS = 20
 
 f = open("words.txt", "r")
 WORDS = set([l.strip() for l in f.readlines() if len(l) <= BOARD_SIZE + 1])
@@ -46,25 +47,23 @@ PASS = 'PASS'
 
 # TODO: write tests for these functions
 
-def check_board(board, edits, across, r, c, first_play):
+def check_board(board, edit_positions, across, r, c):
     def value(l):
         return VALUES.get(l, 0)
     checks = set()
     if across:
         checks.add((r, True))
-        for _, c, _ in edits:
+        for _, c in edit_positions:
             checks.add((c, False))
     else:
         checks.add((c, False))
-        for r, _, _ in edits:
+        for r, _ in edit_positions:
             checks.add((r, True))
     total_score = 0
     scored_words = []
-    connected = False
-    edit_positions = set([(r, c) for r, c, _ in edits])
     for i, across in checks:
         dr, dc = (0, 1) if across else (1, 0)
-        w, score, multiplier, has_old, has_new = '', 0, 1, False, False
+        w, score, multiplier, has_new = '', 0, 1, False
         rr, cc = i * dc, i * dr
         while rr <= BOARD_SIZE and cc <= BOARD_SIZE:
             l = False
@@ -74,83 +73,112 @@ def check_board(board, edits, across, r, c, first_play):
                 w += l
                 if (rr, cc) in edit_positions:
                     has_new = True
-                    m = BOARD[rr][cc]
+                    m = PREMIUMS[rr][cc]
                     if m == 1:
                         multiplier *= 2
                         score += value(l)
                     else:
                         score += value(l) * (m if m != 0 else 1)
                 else:
-                    has_old = True
                     score += value(l)
             elif w:  # reached the end of a word, process it
                 if len(w) >= 2:
-                    if has_old and has_new:
-                        connected = True
                     if w.upper() not in WORDS:
                         return False, None
                     if has_new:
                         total_score += score * multiplier
-                        if len(edits) == RACK_SIZE:
-                            total_score += BINGO_BONUS
                         scored_words.append(w)
-                w, score, multiplier, has_old, has_new = (
-                 '', 0, 1, False, False)
+                w, score, multiplier, has_new = '', 0, 1, False
             rr, cc = rr + dr, cc + dc
-    # Only the first play of the game is allowed to be disconnected.
-    if not connected and not first_play:
-        return False, None
-    return total_score, scored_words
+    if len(edit_positions) == RACK_SIZE:
+        total_score += BINGO_BONUS
+    return total_score, tuple(scored_words)
 
-def check_play(board, r, c, d, p, first_play):
-    def undo_edits(board, edits):
-        for rr, cc, _ in edits:
-            board[rr][cc] = False
-    edits = []
-    new_pos = []
+def check_lane(board, neighbors, r, c, d, max_tiles):
+    placement_info = []
+    connected = False
     rr, cc = r, c
     dr, dc = d
-    # Try to play all the tiles.
-    for i in range(len(p)):
+    results = []
+    # See how many tiles need to be played for connectedness.
+    for i in range(max_tiles):
         while rr < BOARD_SIZE and cc < BOARD_SIZE and board[rr][cc]:
             rr, cc = rr + dr, cc + dc
         if rr == BOARD_SIZE or cc == BOARD_SIZE:
-            undo_edits(board, edits)
-            return False, None, None
-        board[rr][cc] = p[i]
-        edits.append((rr, cc, p[i]))
-    if first_play and not board[BOARD_SIZE//2][BOARD_SIZE//2]:
-        undo_edits(board, edits)
-        return False, None, None
-    score, scored_words = check_board(
-        board, edits, (dc == 1), r, c, first_play)
-    undo_edits(board, edits)
-    return score, edits, scored_words
-    
+            return placement_info
+        if neighbors[rr][cc]:
+            connected = True
+        placement_info.append((rr, cc, connected))
+        # pretend to place a tile
+        rr, cc = rr + dr, cc + dc
+    return placement_info
+
+def neighboring_cells(board):
+    neighbors = [[False for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+    for r in range(BOARD_SIZE):
+        for c in range(BOARD_SIZE):
+            if board[r][c]:
+                for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                    rr, cc = r + dr, c + dc
+                    if (0 <= rr < BOARD_SIZE and 0 <= cc < BOARD_SIZE
+                        and not board[rr][cc]):
+                        neighbors[rr][cc] = True
+    return neighbors
+
 def find_valid_plays(board, rack, first_play):
-    plays = set()
     valid_plays = []
-    for n in range(1, RACK_SIZE + 1):
-        for c in itertools.combinations(rack, n):
-            for p in itertools.permutations(c):
-                if '?' in p:
-                    blank_index = p.index('?')
+    rack_len = len(rack)
+    tile_sets = [set() for _ in range(rack_len)]
+    for num_tiles in range(1, rack_len + 1):
+        for com in itertools.combinations(rack, num_tiles):
+            for perm in itertools.permutations(com):
+                if '?' in perm:
+                    blank_index = perm.index('?')
                     for l in ascii_lowercase:
-                        plays.add(p[0:blank_index] + (l,) + p[blank_index+1:])
+                        tile_sets[num_tiles-1].add(
+                            perm[0:blank_index] + (l,) + perm[blank_index+1:])
                 else:
-                    plays.add(p)
+                    tile_sets[num_tiles-1].add(perm)
+    neighbors = neighboring_cells(board)
     for r in range(BOARD_SIZE):
         for c in range(BOARD_SIZE):
             if board[r][c]:
                 continue
+            # small optimization, could probably remove
+            if first_play and not (r == BOARD_SIZE // 2 or c == BOARD_SIZE // 2):
+                continue
             for d in ((0, 1), (1, 0)):
-                for p in plays:
-                    score, edits, scored_words = check_play(
-                        board, r, c, d, p, first_play)
-                    # Since we only have 1 blank, it is impossible to score 0.
-                    if score:
-                        valid_plays.append(
-                            ValidPlay(score, r, c, p, edits, scored_words))
+                placement_info = check_lane(board, neighbors, r, c, d, rack_len)
+                if first_play:
+                    used_center_square = False
+                    for rr, cc, _ in placement_info:
+                        if rr == BOARD_SIZE // 2 and cc == BOARD_SIZE // 2:
+                            used_center_square = True
+                    if not used_center_square:
+                        continue
+                edit_positions = []
+                across = d[0] == 0
+                for i in range(len(placement_info)):
+                    rr, cc, connected = placement_info[i]
+                    edit_positions.append((rr, cc))
+                    if (not first_play) and (not connected):
+                        continue
+                    for ts in tile_sets[i]:
+                        edits = []
+                        for j in range(i+1):
+                            rrr, ccc = edit_positions[j]
+                            board[rrr][ccc] = ts[j]
+                            edits.append((rrr, ccc, ts[j]))
+                        score, scored_words = check_board(
+                            board, edit_positions, across, r, c)
+                        # Since we only have 1 blank, it is impossible to score 0.
+                        if score:
+                            valid_plays.append(
+                                ValidPlay(
+                                    score, r, c, tuple(edits), scored_words))
+                        for rrr, ccc in edit_positions:
+                            board[rrr][ccc] = False
+                    
     return valid_plays
 
 def draw(bag, rack):
@@ -207,16 +235,16 @@ def greedy_strat(valid_plays, board, rack, unseen):
 
 # Choose a play with the highest score, adjusted to subtract the value of the
 # tiles left in the rack.
-def leave_strat(valid_plays, board, rack, unseen, m=0.1):
+def leave_strat(valid_plays, board, rack, unseen, m=1):
     if not valid_plays:
         return EXCHANGE, None
-    best_score = -1000
+    best_delta = -1000
     best_play = None
     valid_plays.sort()
     valid_plays.reverse()
-    for v in valid_plays[0, min(len(valid_plays), 25)]:
+    for v in valid_plays:
         leave = rack[:]
-        for l in v.p:
+        for _, _, l in v.edits:
             if l.islower():
                 leave.remove('?')
             else:
@@ -226,10 +254,9 @@ def leave_strat(valid_plays, board, rack, unseen, m=0.1):
         penalty = 0
         for l in leave:
             penalty += VALUES[l]
-        # multiplier tentatively chosen via some simulations vs. greedy_strat
-        mod_score = v.score - m * penalty
-        if mod_score > best_score:
-            best_score = mod_score
+        delta = v.score - m * penalty
+        if delta > best_delta:
+            best_delta = delta
             best_play = v
     return PLAY, best_play  
 
@@ -242,10 +269,11 @@ def lookahead_1_strat(valid_plays, board, rack, unseen):
     best_play = None
     valid_plays.sort()
     valid_plays.reverse()
+    # Only look at the highest-scoring valid plays, to save time.
     for v in valid_plays[0 : min(len(valid_plays), 10)]:
         # TODO: this is code from sim(). Factor out to remove redundancy.
         new_board = copy(board)
-        score, _, __, p, edits, ___ = v
+        score, _, __, edits, ___ = v
         for rr, cc, l in edits:
             new_board[rr][cc] = l
         avg_opp_score = 0
@@ -293,11 +321,11 @@ def sim(strat1, strat2, log=False):
         choice, details = strats[active](
             valid_plays, board, racks[active], sorted(racks[1-active] + bag))
         if choice == PLAY:
-            score, r, c, p, edits, scored_words = details
+            score, r, c, edits, scored_words = details
             for rr, cc, l in edits:
                 board[rr][cc] = l
             old_rack = racks[active][:]
-            for l in p:
+            for _, _, l in edits:
                 if l.islower():
                     racks[active].remove('?')
                 else:
@@ -366,4 +394,4 @@ def compare_strats(strat1, strat2, num_trials, log_each_game=False):
     print("Average scores: Player 1 {}, Player 2 {}".format(
         score_totals[0] / num_trials, score_totals[1] / num_trials))
 
-compare_strats(lookahead_1_strat, greedy_strat, 200, log_each_game=False)
+compare_strats(greedy_strat, leave_strat, 1000, log_each_game=False)
